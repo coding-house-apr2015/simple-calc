f4e8b17b5accd7cc903de26a30398e6651c1456f
Jon Edwards <jkedwards@me.com>
2015-04-08 14:13:16 -0700

    close #10

--- client/index.js
+++ client/index.js
@@ -1,2 +1,30 @@
 'use strict';
 console.log('apollo -> online');
+
+$('button').click(function() {
+  if($(this).hasClass('number')) {
+    displayNumber($(this).text());
+  }
+  else if($(this).hasClass('operator')) {
+    handleOperator($(this).text());
+  }
+});
+
+$('#decimal').click(function() {
+  console.log('decimal');
+});
+
+$('#equal').click(function() {
+  console.log('equal');
+});
+
+function displayNumber(newNumber) {
+  var number = $('#display').text();
+  number = (number === '0') ? '' : number;
+  number = number.concat(newNumber);
+  $('#display').text(number);
+}
+
+function handleOperator(operator) {
+
+}
