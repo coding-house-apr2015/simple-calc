b5e4d40d897a1b78743caa6e40fdc111a741580a
Jon Edwards <jkedwards@me.com>
2015-04-08 15:47:15 -0700

    close #35

--- client/index.js
+++ client/index.js
@@ -2,10 +2,15 @@
 
 $(document).ready(init);
 
+var storedNumber = 0;
+var operator = '';
+
 function init(){
   $('.number').click(clickNumber);
   $('#decimal').click(clickDecimal);
   $('#display').click(clear);
+  $('.operator').click(clickOperator);
+  $('#equal').click(performCalculation);
 }
 
 function clickNumber(){
@@ -24,4 +29,31 @@ function clickDecimal(){
 
 function clear(){
   $('#display').text('0');
+}
+
+function clickOperator(){
+  storedNumber = parseFloat($('#display').text());
+  operator = $(this).text();
+  clear();
+}
+
+function performCalculation() {
+  var newNumber = parseFloat($('#display').text());
+  var result;
+  console.log(operator);
+  switch (operator) {
+    case '+':
+      result = storedNumber + newNumber;
+      break;
+    case '-':
+      result = storedNumber - newNumber;
+      break;
+    case '×':
+      result = storedNumber * newNumber;
+      break;
+    case '÷':
+      result = storedNumber / newNumber;
+  }
+  $('#display').text(result);
+  storedNumber = 0;
 }
