0f7a012faa6b442492c2d56ddf12e2e375f8f26a
Dick <rljarm@gmail.com>
2015-04-08 14:14:26 -0700

    corrected buttons

--- client/index.js
+++ client/index.js
@@ -1,2 +1,10 @@
 'use strict';
 console.log('apollo -> online');
+$(document).ready(init);
+
+function init(){
+  $('.number').click(function(){
+    $('#display').append($(this).text());
+  });
+
+}
