d0c304a8385363ce0e8d92abe218408c1cfdde8b
Chris Oyler <christopher.oyler@gmail.com>
2015-04-08 16:08:36 -0700

    working calculator

--- client/index.js
+++ client/index.js
@@ -6,6 +6,8 @@ function init(){
   $('.number').click(clickNumber);
   $('#decimal').click(clickDecimal);
   $('#display').click(clear);
+  $('.operator').click(op);
+  $('#equal').click(calc);
 }
 
 function clickNumber(){
@@ -24,4 +26,31 @@ function clickDecimal(){
 
 function clear(){
   $('#display').text('0');
+}
+
+var number;
+var operator;
+
+function op(){
+  number = $('#display').text();
+  operator = $(this).text();
+  $('#display').text('0');
+}
+
+function calc(){
+  var result;
+  switch(operator) {
+    case '+':
+      result = (parseFloat((number * 1) + ($('#display').text() * 1))).toString();
+      break;
+    case '-':
+      result = (parseFloat((number * 1) - ($('#display').text() * 1))).toString();
+      break;
+    case '×':
+      result = (parseFloat((number * 1) * ($('#display').text() * 1))).toString();
+      break;
+    case '÷':
+      result = (parseFloat((number * 1) / ($('#display').text() * 1))).toString();
+  }
+  $('#display').text(result);
 }
