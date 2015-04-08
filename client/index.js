b41f763872fa611738017831716a0eeffa8fb4e1
Jon Edwards <jkedwards@me.com>
2015-04-08 14:56:22 -0700

    close #20

--- client/index.js
+++ client/index.js
@@ -4,11 +4,18 @@ $(document).ready(init);
 
 function init(){
   $('.number').click(clickNumber);
+  $('#decimal').click(decimalHandler);
 }
 
 function clickNumber(){
   var num = $(this).text();
   var display = $('#display').text();
   var output = (display === '0') ? num : display + num;
+  $('#display').text(output);
+}
+
+function decimalHandler() {
+  var display = $('#display').text();
+  var output = (display.search(/[.]/) < 0) ? display + '.' : display;
   $('#display').text(output);
 }
