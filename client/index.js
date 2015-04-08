5d3b0fbe5d246248ec2c9d16d3a585e762182db1
Jon Edwards <jkedwards@me.com>
2015-04-08 14:36:33 -0700

    add Chyld's code

--- client/index.js
+++ client/index.js
@@ -1,6 +1,19 @@
 'use strict';
-console.log('apollo -> online');
 
+$(document).ready(init);
+
+function init() {
+  $('.number').click(clickNumber);
+}
+
+function clickNumber() {
+  var num = $(this).text();
+  var display = $('#display').text();
+  var output = (display === '0') ? num : display + num;
+  $('#display').text(output);
+}
+
+/*
 $('button').click(function() {
   if($(this).hasClass('number')) {
     displayNumber($(this).text());
@@ -32,3 +45,4 @@ function displayNumber(newNumber) {
 function handleOperator(operator) {
   // handle operator
 }
+*/
