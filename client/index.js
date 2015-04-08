b8a8b8514ea5ad54b5fe0c081a0800ed62e7e9f5
Clyde G Marl <marl.clyde@gmail.com>
2015-04-08 14:05:14 -0700

    add display functionality for numbers

--- client/index.js
+++ client/index.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  $('.number').click(function(){
+    display($(this).text());
+  });
+}
+
+function display(newText){
+  var current = $('#display').text();
+  switch (current){
+    case '0':
+      $('#display').text(newText);
+      break;
+    case '.':
+      $('#display').text(current + newText);
+      break;
+    default:
+      $('#display').text(current + newText);
+  }
+}
