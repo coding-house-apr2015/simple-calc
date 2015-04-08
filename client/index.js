424058f83743c96e9be08241a4d5e23549dad123
Chris Oyler <christopher.oyler@gmail.com>
2015-04-08 14:12:34 -0700

    working number buttons

--- client/index.js
+++ client/index.js
@@ -1,2 +1,17 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(go);
+function go(){
+
+
+var number = '';
+var newNumber = '';
+var display = $('#display');
+
+display.text('0');
+$('.number').click(function(){
+  number += $(this).text();
+  display.text(number);
+});
+
+}
