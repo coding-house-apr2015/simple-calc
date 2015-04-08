638ff28a1f3a4b4970a058589eae8abd71d75095
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-08 14:08:37 -0700

    resolve #10

--- client/index.js
+++ client/index.js
@@ -1,2 +1,37 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+var displayStr = '0';
+var displayNum = 0;
+
+function init(){
+  $('.number').click(updateDisplay);
+  $('#display').click(clear);
+}
+
+// function clear(){
+//   //var currentFontSize = $('#display').css('font-size');
+//   // if (currentFontSize !== '50px'){
+//   //   $('#display').css('font-size', '50px');
+//   // }
+//   displayStr = '0';
+//   displayNum = 0;
+//   $('#display').text(displayStr);
+// }
+
+function updateDisplay(){
+  var btnValue = $(this).text().trim();
+  if (btnValue === '.') {
+    displayStr += btnValue;
+  }
+  else if (displayNum === 0){
+    displayStr = btnValue;
+  }
+  else {
+    displayStr += btnValue;
+  }
+
+  displayNum =  parseFloat(displayStr);
+  $('#display').text(displayStr);
+}
